d19d514806135b3c640649a77e9c52204d3ec847
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-07-18 00:39:32 +0300

    IUnitOfWork add

--- RadioKPIBot.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ RadioKPIBot.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RadioKPIBot.DataAccess.Repository.IRepository
 {
-    internal interface IUnitOfWork
+    public interface IUnitOfWork
     {
         IAdminRepository Admin { get; }
 

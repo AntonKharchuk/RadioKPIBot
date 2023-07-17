a34352f9cd3c63d03c9c1cdb71f711c48f156468
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-07-18 01:40:54 +0300

    GetButtonWithCallBackData

--- RadioKPIBot/BotMethods.cs
+++ RadioKPIBot/BotMethods.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RadioKPIBot.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace RadioKPIBot
 {
     public class BotMethods
     {
+        private InlineKeyboardButton GetButtonWithCallBackData(string text, CallBackData data)
+        {
+            var jsonCallBack = JsonConvert.SerializeObject(data);
+            return InlineKeyboardButton.WithCallbackData(text, jsonCallBack);
+        }
+
+        public InlineKeyboardMarkup GetAdminChoiseMarkup(string id)
+        {
+
+            List<List<InlineKeyboardButton>> buttons = new List<List<InlineKeyboardButton>> { };
+
+            CallBackData callBackAllow = new CallBackData()
+            {
+                Act = "AllowTreck",
+                Value = id
+            };
+          
+            CallBackData callBackNotAllow = new CallBackData()
+            {
+                Act = "NotAllowTreck",
+                Value = id
+            };
+
+            buttons.Add(new List<InlineKeyboardButton>
+                        {
+                            GetButtonWithCallBackData("AllowTreck",callBackAllow),
+                            GetButtonWithCallBackData("NotAllowTreck",callBackNotAllow)
+                        }
+            );
+
+            InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup
+            (
+            buttons
+            );
+            return keyboardMarkup;
+        }
+
+
     }
 }

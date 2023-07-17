c374c414b86ddd01783bee63d9f5920e65d94b6e
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-07-18 00:48:31 +0300

    add CallBackData

---
+++ RadioKPIBot.Models/CallBackData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioKPIBot.Models
+{
+    public class CallBackData
+    {
+        public string Act { get; set; }
+        public string Value { get; set; }
+    }
+}

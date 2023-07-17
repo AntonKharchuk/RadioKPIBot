1b955276d05460b504eecf01930fd5ecb1a3bb1d
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-07-18 01:26:01 +0300

    add Unit of work inmlementanion

---
+++ RadioKPIBot.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using RadioKPIBot.DataAccess.Repository.IRepository;
+using RadioKPIBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioKPIBot.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private AppDbContext _db;
+        public IAdminRepository Admin{ get; private set; }
+
+
+        public UnitOfWork(AppDbContext db)
+        {
+            _db = db;
+            Admin = new AdminRepository(db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}

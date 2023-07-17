1b90449887859c934334867d0f9d8d2c425cbc28
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-07-17 17:27:55 +0300

    add AdminRepository

---
+++ RadioKPIBot.DataAccess/Repository/AdminRepository.cs
@@ -0,0 +1,26 @@
+﻿using RadioKPIBot.DataAccess.Repository.IRepository;
+using RadioKPIBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioKPIBot.DataAccess.Repository
+{
+   
+    public class AdminRepository : Repository<Admin>, IAdminRepository
+    {
+        private AppDbContext _db;
+
+        public AdminRepository(AppDbContext db) : base(db)  
+        {
+            _db = db;
+        }
+
+        public void Update(Admin obj)
+        {
+            _db.UserData.Update(obj);
+        }
+    }
+}

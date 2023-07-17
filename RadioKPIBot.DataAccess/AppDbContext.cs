886039367e6726aa5bc574ba6125dc04efbbe4d5
Anton Kharchuk <Kharchuk.anton@gmail.com>
2023-07-17 17:25:19 +0300

    Add AppDbContext and Repository

---
+++ RadioKPIBot.DataAccess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RadioKPIBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioKPIBot.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Admin> UserData { get; set; }
+    }
+}
---
+++ RadioKPIBot.DataAccess/Repository/Repository.cs
@@ -0,0 +1,66 @@
+﻿using RadioKPIBot.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioKPIBot.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        private readonly AppDbContext _db;
+
+        internal DbSet<T> _dbSet;
+
+        public Repository(AppDbContext db, string? includeProps = null)
+        {
+            _db = db;
+            _dbSet = db.Set<T>();
+        }
+
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll(string? includeProps = null)
+        {
+            IQueryable<T> quary = _dbSet;
+            if (includeProps != null)
+            {
+                foreach (var item in includeProps.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    quary = quary.Include(item);
+                }
+            }
+
+            return quary.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProps = null)
+        {
+            IQueryable<T> quary = _dbSet;
+            quary = quary.Where(filter);
+            if (includeProps != null)
+            {
+                foreach (var item in includeProps.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                {
+                    quary = quary.Include(item);
+                }
+            }
+
+            return quary.FirstOrDefault();
+
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
